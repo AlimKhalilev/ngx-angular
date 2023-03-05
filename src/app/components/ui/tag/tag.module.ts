86c68a267b8df1e5b270fd227702116684827491
AlimKhalilev <alim-halilko@mail.ru>
2023-03-05 13:58:16 +0300

    доработка тэга, тест функционала text-ellipsis у текста, где обрезан текст

--- src/app/components/ui/tag/tag.module.ts
+++ src/app/components/ui/tag/tag.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxTagComponent } from './tag.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
     declarations: [NgxTagComponent],
-    imports: [CommonModule, MatIconModule],
+    imports: [CommonModule, MatIconModule, MatTooltipModule],
     exports: [NgxTagComponent]
 })
 export class NgxTagModule {}
